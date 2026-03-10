cf138ba64842d4667ca7abcde89e9bd0b6e99d50
Bekzod <tobby4173@gmail.com>
2026-03-10 19:51:00 +0500

    qildim

--- src/apps/product/admin.py
+++ src/apps/product/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product, Category, ProductInput
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(ProductInput)
+
+admin.site.register(Category)
